9c66da38e98c25268b41fc95d1f74352d798ddb2
keeganasmith <99233684+keeganasmith@users.noreply.github.com>
2022-07-26 07:56:28 -0500

    insert, remove, at, size methods

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -21,9 +21,13 @@ public:
     void removeHead();
     void push_front(T val);
     void add(T val, size_t index);
-    size_t size() const;
+    size_t size() const{
+        return _size;
+    }
     const Node<T>* head() const;
     void clear();
+    const T at(size_t index) const;
+    T& at(size_t index);
 };
 template <class T>
 const Node<T>* LinkedList<T>::head() const{
@@ -94,5 +98,84 @@ LinkedList<T>::LinkedList(const LinkedList<T>& other): LinkedList(){
         other_temp = other_temp->next;
         _size++;
     }
+}
+template <class T>
+LinkedList<T>& LinkedList<T>::operator=(const LinkedList<T>& other){
+    if(this == &other){
+        return *this;
+    }
+    this->clear();
+    const Node<T>* other_head = other.head();
+    if(other_head == nullptr){
+        return *this;
+    }
+    _head = new Node(other_head->val);
+    _size++;
+    Node<T>* temp = _head;
+    Node<T>* other_temp = other_head->next;
+    while(other_temp != nullptr){
+        temp->next = new Node(other_temp->val);
+        temp = temp->next;
+        other_temp = other_temp->next;
+        _size++;
+    }
+    return *this;
+}
+template <class T>
+void LinkedList<T>::removeHead(){
+    if(_head == nullptr || _size == 0){
+        throw std::out_of_range("Out of bounds!");
+    }
+    Node<T>* temp = _head;
+    _head = _head->next;
+    delete temp;
+    
+    _size--;
+}
+template <class T>
+void LinkedList<T>::add(T val, size_t index){
+    if(index > _size){
+        throw std::out_of_range("out of bounds!");
+    }
+    if(index == 0){
+        this->push_front(val);
+        return;
+    }
+    unsigned int i = 0;
+    Node<T>* temp = _head;
+    while(i < index-1){
+        temp = temp->next;
+        i++;
+    }
+    Node<T>* temp2 = temp->next;
+    temp->next = new Node(val);
+    temp->next->next = temp2;
+    _size++;
 
+}
+template <class T>
+const T LinkedList<T>::at(size_t index) const{
+    if(index >= _size){
+        throw std::out_of_range("out of bounds!");
+    }
+    unsigned int i = 0;
+    Node<T>* temp = _head;
+    while(i < index){
+        temp = temp->next;
+        i++;
+    }
+    return temp->val;
+}
+template <class T>
+T& LinkedList<T>::at(size_t index){
+    if(index >= _size){
+        throw std::out_of_range("out of bounds!");
+    }
+    unsigned int i = 0;
+    Node<T>* temp = _head;
+    while(i < index){
+        temp = temp->next;
+        i++;
+    }
+    return temp->val;
 }
--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,6 +1,44 @@
 #include "LinkedList.cpp"
 using std::cout, std::endl;
-
+void test_at(){
+    LinkedList<int> joe;
+    joe.add(0,0);
+    joe.add(1,1);
+    joe.add(2,2);
+    cout << "joe: " << joe << "\n";
+    cout << "joe[0] = " << joe.at(0) << "\n";
+    joe.at(0) = 5;
+    cout << "joe[0] = " << joe.at(0) << "\n";
+    for(unsigned int i = 0; i < joe.size();i++){
+        cout << joe.at(i) << " ";
+    }
+    cout << "\n";
+}
+void test_insert(){
+    LinkedList<int> joe;
+    joe.add(1,0);
+    cout << joe << "\n";
+    joe.add(3,0);
+    joe.add(2,1);
+    cout << joe << "\n";
+    joe.add(0,3);
+    cout << joe << "\n";
+    
+}
+void test_remove_head(){
+    LinkedList<int> joe;
+    joe.push_front(3);
+    joe.push_front(2);
+    joe.push_front(1);
+    cout << "Joe: " << joe << "\n";
+    joe.removeHead();
+    cout << "Joe: " << joe << "\n";
+    joe.removeHead();
+    cout << "Joe: " << joe << "\n";
+    joe.removeHead();
+    cout << "Joe: " << joe << "\n";
+    joe.removeHead();
+}
 void test_copy_constructor(){
     LinkedList<int> joe;
     joe.push_front(1);
@@ -14,6 +52,24 @@ void test_copy_constructor(){
     cout << "joe: " << joe << "\n";
     cout << "bob: " << bob << "\n";
 }
+void test_copy_assignment_operator(){
+    LinkedList<int> joe;
+    joe.push_front(1);
+    joe.push_front(2);
+    joe.push_front(3);
+    LinkedList<int> bob;
+    bob.push_front(4);
+    bob.push_front(5);
+    bob.push_front(6);
+    cout << "joe: " << joe << "\n";
+    cout << "bob: " << bob << "\n";
+    joe = bob;
+    cout << "joe: " << joe << "\n";
+    cout << "bob: " << bob << "\n";
+    if(&joe == &bob){
+        cout << "FAIL\n";
+    }
+}
 void test_add(){
     LinkedList<int> joe;
     joe.push_front(1);
@@ -24,5 +80,9 @@ void test_add(){
 }
 int main(){
     //test_add();
-    test_copy_constructor();
+    //test_copy_constructor();
+    //test_copy_assignment_operator();
+    //test_remove_head();
+    //test_insert();
+    test_at();
 }
